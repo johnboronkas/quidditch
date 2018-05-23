fba840aaf5fda5d8be3271a7f7608c0f02b46cd0
John Boronkas <johnboronkas@gmail.com>
2018-05-22 21:48:54 -0400

    Added keyboard movement for debug

--- quidditch/Scripts/Player.gd
+++ quidditch/Scripts/Player.gd
@@ -4,10 +4,14 @@ export (int) var SPEED
 export (float) var ROTATION_SPEED
 export (int) var ACC_LIMIT
 
+var acc_mode = false
+
 func _process(delta):
-	var x_tilt = clamp(Input.get_accelerometer().x, -ACC_LIMIT, ACC_LIMIT)
-	var y_tilt = clamp(-Input.get_accelerometer().y, -ACC_LIMIT, ACC_LIMIT)
-	var velocity = Vector2(x_tilt, y_tilt)
+	var velocity = Vector2()
+	if acc_mode:
+		velocity = get_acc_velocity()
+	else:
+		velocity = get_kb_velocity()
 
 	# Only rotate towards direction of movement if velocity is fast.
 	if abs(velocity.x) > 1 || abs(velocity.y) > 1:
@@ -22,3 +26,22 @@ func _process(delta):
 				rotation_degrees -= ROTATION_SPEED
 
 	position += velocity * SPEED * delta
+
+func get_acc_velocity():
+	var x_tilt = clamp(Input.get_accelerometer().x, -ACC_LIMIT, ACC_LIMIT)
+	var y_tilt = clamp(-Input.get_accelerometer().y, -ACC_LIMIT, ACC_LIMIT)
+	return Vector2(x_tilt, y_tilt)
+
+func get_kb_velocity():
+	var velocity = Vector2()
+
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += ACC_LIMIT
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= ACC_LIMIT
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += ACC_LIMIT
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= ACC_LIMIT
+
+	return velocity
