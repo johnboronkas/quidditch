51869cb68a26b4524770f6b09396aad569e374f3
John Boronkas <johnboronkas@gmail.com>
2018-05-22 20:37:54 -0400

    First attempt at fixing rotation (not rotating right now)

--- quidditch/Scripts/Player.gd
+++ quidditch/Scripts/Player.gd
@@ -1,27 +1,24 @@
 extends Area2D
 
 export (int) var SPEED
+export (float) var ROTATION_SPEED
 export (int) var ACC_LIMIT
-
-var look_dir_lerp_value
-var last_look_dir
-
-func _ready():
-	look_dir_lerp_value = 0
-	last_look_dir = 0
 
 func _process(delta):
 	var x_tilt = clamp(Input.get_accelerometer().x, -ACC_LIMIT, ACC_LIMIT)
 	var y_tilt = clamp(-Input.get_accelerometer().y, -ACC_LIMIT, ACC_LIMIT)
 	var velocity = Vector2(x_tilt, y_tilt)
 
-	var look_dir = position.angle_to(-velocity)
-	if last_look_dir != look_dir:
-		look_dir_lerp_value = 0
-	last_look_dir = look_dir
-	rotation = lerp(last_look_dir, look_dir, look_dir_lerp_value)
-	look_dir_lerp_value += 0.2
-	if look_dir_lerp_value > 1:
-		look_dir_lerp_value = 1
+	# Only rotate towards direction of movement if velocity is fast.
+	if abs(velocity.x) > 1 || abs(velocity.y) > 1:
+		var desired_look_dir = position.angle_to(-velocity)
+		var look_dir_diff = rotation_degrees - desired_look_dir
+
+		# Close enough to looking towards desired look direction, so stop rotation.
+		if abs(look_dir_diff) > 2:
+			if look_dir_diff < 0:
+				rotation_degrees += ROTATION_SPEED
+			else:
+				rotation_degrees -= ROTATION_SPEED
 
 	position += velocity * SPEED * delta
