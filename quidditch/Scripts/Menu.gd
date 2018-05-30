c36db1ee12ab8e5471a29066d71c2eac89fb44b6
John Boronkas <johnboronkas@gmail.com>
2018-05-29 23:00:34 -0400

    Added team and position selector UI

--- quidditch/Scripts/Menu.gd
+++ quidditch/Scripts/Menu.gd
@@ -1,20 +1,44 @@
 extends VBoxContainer
 
-func _ready():
-	pass
+var teams = []
+var teamIndex = 0
+var positions = []
+var positionIndex = 0
 
+func _ready():
+	populateTeams()
+	populatePositions()
+
+func populateTeams():
+	teams.append("red")
+	teams.append("green")
+	teams.append("blue")
+	teams.append("gold")
+	$HBoxContainer/TeamButton.text = teams[teamIndex]
+
+func populatePositions():
+	positions.append("seeker")
+	positions.append("chaser")
+	positions.append("beater")
+	positions.append("keeper")
+	$HBoxContainer/PositionButton.text = positions[positionIndex]
+
+func _on_TeamButton_pressed():
+	teamIndex = (teamIndex + 1) % teams.size()
+	$HBoxContainer/TeamButton.text = teams[teamIndex]
+
+func _on_PositionButton_pressed():
+	positionIndex = (positionIndex + 1) % positions.size()
+	$HBoxContainer/PositionButton.text = positions[positionIndex]
 
 func _on_HostButton_pressed():
 	$StatusLabel.text = "hosting 1/14 players"
 	$PlayButton.disabled = false
-	pass # replace with function body
 
 
 func _on_JoinButton_pressed():
 	$StatusLabel.text = "joining 2/14 players"
-	pass # replace with function body
 
 
 func _on_PlayButton_pressed():
 	get_tree().change_scene("res://Scenes/Game.tscn")
-	pass # replace with function body
